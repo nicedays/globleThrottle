a435a428ec63e2e6d42fb0895f3040527b22acb8
nicedays <ikevinsama@gmail.com>
2022-01-03 17:53:31 +0800

    说明

---
+++ src/components/test.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="main">
+		<button v-throttle="2000" @click="myClick">submit</button>
+	</div>
+</template>
+
+<script>
+	export default {
+	  name: 'test',
+	  data(){
+		  return{
+			  
+		  }
+	  },
+	  methods:{
+		  myClick(){
+			  console.log('1231231')
+		  }
+	  }
+	  
+	}
+</script>
+
+<style>
+	.main{
+		margin: auto auto;
+		width: 50px;
+		height: 10px;
+	}
+</style>
---
+++ src/directive/throttle.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+const Doms = []
+Vue.directive('throttle', {
+    inserted(el, delay) {
+        Doms.push(el) // 使用el这个dom对象
+        el.addEventListener('click', () => {
+            // 禁用这个指令的DOM结构点击事件
+            Doms.forEach(item => {
+                item.style.pointerEvents = 'none';
+            });
+            setTimeout(() => {
+                // 启动这个指令的DOM结构点击事件
+				Doms.forEach(item => {
+                item.style.pointerEvents = 'auto';
+            });
+            }, delay.value || 1000); // 默认1秒
+        });
+    }
+});
